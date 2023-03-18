b79ed97b5879261832587ee7418550cf9a34b586
Imed Bouchrika <imed@imed.ws>
2023-03-18 13:34:38 +0000

    Adding the user signup view

---
+++ view/user/signup.php
@@ -0,0 +1,45 @@
+<div class="signup-box">
+    <div class="card card-outline card-primary">
+        <div class="card-header text-center"><?php echo LANG_APP_NAME;?></div>
+        <div class="card-body">
+            <p class="login-box-msg"><?php echo LANG_SIGNUP;?></p>
+            <form action="index.php?action=do_login" method="post">
+
+                <div class="input-group mb-3">
+                    <input type="name" class="form-control" placeholder="<?php echo LANG_YOUR_NAME;?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-user"></span>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="input-group mb-3">
+                    <input type="email" class="form-control" placeholder="<?php echo LANG_EMAIL;?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="input-group mb-3">
+                    <input type="password" class="form-control" placeholder="<?php echo LANG_PASSWORD;?>">
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-lock"></span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-4">
+                	<button type="submit" class="btn btn-primary"><?php echo LANG_SIGNUP;?></button>
+                </div>
+            </form>
+        </div>
+        <p class="m-1">
+        	<a href="index.php?action=login" class="text-center"><?php echo LANG_LOGIN_HAVE_ACCOUNT;?></a>
+        </p>
+    </div>
+</div>
+
