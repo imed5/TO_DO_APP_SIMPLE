5f7c19d9fbd55672e97c38db00fbc406d835b02a
Imed Bouchrika <imed@imed.ws>
2023-03-18 14:52:19 +0000

    Login BL + Fix bug for hashing passwords of signup

--- lib/func.user.php
+++ lib/func.user.php
@@ -1,5 +1,40 @@
 <?php
 
+function user_process_login($vars){
+    global $db;
+
+	$ret['status']=0;
+	$ret['error']='';
+	
+	$vars['email']=trim(strtolower($vars['email']));
+	
+    if (strlen($ret['error'])==0 and strlen($vars['email'])==0) {
+        $ret['error']="You need to provide an email.";
+        return $ret;
+    }
+
+    if (strlen($ret['error'])==0 and strlen($vars['pass'])==0) {
+        $ret['error']="The password should be filled.";
+        return $ret;
+    }
+
+    if (strlen($ret['error'])>0)return  $ret;
+    
+    //search for it in the database ?
+	$items = $db->query("SELECT * FROM users WHERE LOWER(email) = ? and pass= ?",$vars['email'], md5($vars['pass']))->fetchAll();
+	if (count($items)==0){
+	        $ret['error']="Incorrect Email or Password";
+	        return $ret;
+	}
+	//For the sake of simplicity, log the user directly by setting their cookies..
+	setcookie("app_email", $vars['email'], time()+(3600*24),"/");
+	setcookie("app_pass", md5($vars['pass']), time()+(3600*24),"/");
+	
+	$ret['status']=1;
+	$ret['error']='';
+	return $ret;
+}
+
 function user_process_signup($vars){
 	global $db;
 	
@@ -30,7 +65,7 @@ function user_process_signup($vars){
 	        return $ret;
 	}
 	//Else, there is no users in the db with the same email
-    $db->query("INSERT INTO users (name, email, pass) VALUES ( ?, ?, ? )", $vars['name'], $vars['email'], $vars['pass']);
+    $db->query("INSERT INTO users (name, email, pass) VALUES ( ?, ?, ? )", $vars['name'], $vars['email'], md5($vars['pass']));
 				
 	//log the user directly by setting their cookies..
 	setcookie("app_email", $vars['email'], time()+(3600*24),"/");
--- modules/user.php
+++ modules/user.php
@@ -28,7 +28,15 @@ switch($vars['action']){
     }break;    
 
     case "do_login":{
-        
+        print_r($vars);
+         $ret=user_process_login($vars);
+        print_r($ret); 
+         if ($ret['status']==1){
+            header("location: index.php?action=list"); 
+         }else{
+            header("location: index.php?action=login&error_message=".urlencode($ret['error']));
+         }
+         exit;        
     }break;    
     
     case "logout":{
