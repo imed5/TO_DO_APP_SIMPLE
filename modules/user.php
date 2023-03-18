bc7c52a335c81f4342220b5f8e17cad052fca84b
Imed Bouchrika <imed@imed.ws>
2023-03-18 14:40:30 +0000

    adding the ligin view

--- modules/user.php
+++ modules/user.php
@@ -10,11 +10,21 @@ switch($vars['action']){
     }break;
     
     case "do_signup":{
-        
+         $ret=user_process_signup($vars);
+         
+         if ($ret['status']==1){
+            header("location: index.php?action=list"); 
+         }else{
+            header("location: index.php?action=signup&error_message=".urlencode($ret['error']));
+         }
+         exit;
     }break;
     
     case "login":{
-
+        include("view/header.php");
+        include("view/user/login.php");
+        include("view/footer.php");
+        exit;
     }break;    
 
     case "do_login":{
