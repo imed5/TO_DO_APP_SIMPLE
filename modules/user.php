fd00082b00c51c64426e9ced959f0e153163192a
Imed Bouchrika <imed@imed.ws>
2023-03-18 13:07:19 +0000

    Adding USer.php Controller

---
+++ modules/user.php
@@ -0,0 +1,26 @@
+<?php
+
+switch($vars['action']){
+    
+    case "signup":{
+        
+    }break;
+    
+    case "do_signup":{
+        
+    }break;
+    
+    case "login":{
+
+    }break;    
+
+    case "do_login":{
+        
+    }break;    
+    
+    case "logout":{
+        
+    }break;    
+    
+}
+?>
